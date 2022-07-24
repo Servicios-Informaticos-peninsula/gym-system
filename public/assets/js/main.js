7e285f74c631eb8a9550cb43336b1722e78b3203
Rodrigo Diaz Serviran <diaz-rodrigo@hotmail.com>
2022-07-23 20:49:43 -0500

    adaptacion del template

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,154 +1,51 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
-/******/ })
-/************************************************************************/
-/******/ ({
+function slideToggle(t,e,o){0===t.clientHeight?j(t,e,o,!0):j(t,e,o)}function slideUp(t,e,o){j(t,e,o)}function slideDown(t,e,o){j(t,e,o,!0)}function j(t,e,o,i){void 0===e&&(e=400),void 0===i&&(i=!1),t.style.overflow="hidden",i&&(t.style.display="block");var p,l=window.getComputedStyle(t),n=parseFloat(l.getPropertyValue("height")),a=parseFloat(l.getPropertyValue("padding-top")),s=parseFloat(l.getPropertyValue("padding-bottom")),r=parseFloat(l.getPropertyValue("margin-top")),d=parseFloat(l.getPropertyValue("margin-bottom")),g=n/e,y=a/e,m=s/e,u=r/e,h=d/e;window.requestAnimationFrame(function l(x){void 0===p&&(p=x);var f=x-p;i?(t.style.height=g*f+"px",t.style.paddingTop=y*f+"px",t.style.paddingBottom=m*f+"px",t.style.marginTop=u*f+"px",t.style.marginBottom=h*f+"px"):(t.style.height=n-g*f+"px",t.style.paddingTop=a-y*f+"px",t.style.paddingBottom=s-m*f+"px",t.style.marginTop=r-u*f+"px",t.style.marginBottom=d-h*f+"px"),f>=e?(t.style.height="",t.style.paddingTop="",t.style.paddingBottom="",t.style.marginTop="",t.style.marginBottom="",t.style.overflow="",i||(t.style.display="none"),"function"==typeof o&&o()):window.requestAnimationFrame(l)})}
 
-/***/ "./src/assets/js/main.js":
-/*!*******************************!*\
-  !*** ./src/assets/js/main.js ***!
-  \*******************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+let sidebarItems = document.querySelectorAll('.sidebar-item.has-sub');
+for(var i = 0; i < sidebarItems.length; i++) {
+    let sidebarItem = sidebarItems[i];
+	sidebarItems[i].querySelector('.sidebar-link').addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        let submenu = sidebarItem.querySelector('.submenu');
+        if( submenu.classList.contains('active') ) submenu.style.display = "block"
 
-// Dropdown Sidebar Menu
-var sidebarItems = document.querySelectorAll('.sidebar-item.has-sub');
-
-var _loop = function _loop() {
-  var sidebarItem = sidebarItems[i];
-  sidebarItems[i].querySelector('.sidebar-link').addEventListener('click', function (e) {
-    e.preventDefault();
-    var submenu = sidebarItem.querySelector('.submenu');
-    if (submenu.classList.contains('active')) submenu.classList.remove('active');else submenu.classList.add('active');
-  });
-};
-
-for (var i = 0; i < sidebarItems.length; i++) {
-  _loop();
-} // Navbar Toggler
-
-
-var sidebarToggler = document.querySelectorAll(".sidebar-toggler");
-
-for (var i = 0; i < sidebarToggler.length; i++) {
-  var toggler = sidebarToggler[i];
-  toggler.addEventListener('click', function () {
-    var sidebar = document.getElementById('sidebar');
-    if (sidebar.classList.contains('active')) sidebar.classList.remove('active');else sidebar.classList.add('active');
-  });
-} // Perfect Scrollbar INit
-
-
-if (typeof PerfectScrollbar == 'function') {
-  var container = document.querySelector(".sidebar-wrapper");
-  var ps = new PerfectScrollbar(container);
+        if( submenu.style.display == "none" ) submenu.classList.add('active')
+        else submenu.classList.remove('active')
+        slideToggle(submenu, 300)
+    })
 }
 
-window.onload = function () {
-  var w = window.innerWidth;
+window.addEventListener('DOMContentLoaded', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        document.getElementById('sidebar').classList.remove('active');
+    }
+});
+window.addEventListener('resize', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        document.getElementById('sidebar').classList.remove('active');
+    }else{
+        document.getElementById('sidebar').classList.add('active');
+    }
+});
 
-  if (w < 768) {
-    console.log('widthnya ', w);
-    document.getElementById('sidebar').classList.remove('active');
-  }
-};
+document.querySelector('.burger-btn').addEventListener('click', () => {
+    document.getElementById('sidebar').classList.toggle('active');
+})
+document.querySelector('.sidebar-hide').addEventListener('click', () => {
+    document.getElementById('sidebar').classList.toggle('active');
 
-feather.replace();
-
-/***/ }),
-
-/***/ 1:
-/*!*************************************!*\
-  !*** multi ./src/assets/js/main.js ***!
-  \*************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(/*! D:\Programmer\Code\HTML_CSS\Voler\src\assets\js\main.js */"./src/assets/js/main.js");
+})
 
 
-/***/ })
+// Perfect Scrollbar Init
+if(typeof PerfectScrollbar == 'function') {
+    const container = document.querySelector(".sidebar-wrapper");
+    const ps = new PerfectScrollbar(container, {
+        wheelPropagation: false
+    });
+}
 
-/******/ });
+// Scroll into active sidebar
+document.querySelector('.sidebar-item.active').scrollIntoView(false)
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,35 @@
+
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="d-flex justify-content-between">
+                <div class="logo">
+                    <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo"
+                            srcset=""></a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i
+                            class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title"></li>
+
+                <li class="sidebar-item ">
+                    <a href="" class='sidebar-link'>
+                        <i class=""></i>
+                        <span></span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href=""></a>
+                        </li>
+                    </ul>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+
+
