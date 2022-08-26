f97948a03fde34ddd013c2ab714c3a74ab5330c8
Pedro Adrian Sanchez Cardenas <pedrosanchezcardenas@gmail.com>
2022-08-25 19:46:55 -0500

    Update User Model and create UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* Super-Admin */
+        User::create([
+            'name' => 'Pedro Sánchez Cárdenas',
+            'email' => 'it@domain.com',
+            'phone' => '9999708319',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
