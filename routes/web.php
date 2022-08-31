03ad126834e6b31c102217809e99eedc376b898b
Pedro Adrian Sanchez Cardenas <pedrosanchezcardenas@gmail.com>
2022-08-30 22:21:33 -0500

    Auto stash before merge of "development" and "origin/development"

--- routes/web.php
+++ routes/web.php
@@ -19,27 +19,34 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-//Auth::routes();
-Auth::routes(['register' => true,
-    'login' => true,
-    'password/confirm' => false,
-    'password/reset' => false,
-]);
-Route::group(['middleware' => ['auth', 'verified']], function () {
-    /**Rutas del home */
+Auth::routes(['register' => true, 'login' => true, 'password/confirm' => false, 'password/reset' => false]);
 
+/*
+ * Home Routes
+ */
+Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::get('/punto/venta', [App\Http\Controllers\HomeController::class, 'index'])->name('sales.point');
         Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
     });
 
+    Route::controller('ProviderController')
+        ->prefix('proveedores/')
+        ->group(function () {
+            Route::get('lista', 'index')->name('provider.index');
+            Route::post('registro', 'store')->name('provider.store');
+            Route::put('modificar/{id}', 'update')->name('provider.update');
+            Route::delete('eliminar/{id}', 'destroy')->name('provider.destroy');
+        });
 
-    Route::controller('ProviderController')->prefix('proveedores/')->group(function () {
-        Route::get('lista',  'index')->name('provider.index');
-        Route::post('registro',  'store')->name('provider.store');
-        Route::put('modificar/{id}', 'update')->name('provider.update');
-        Route::delete('eliminar/{id}', 'destroy')->name('provider.destroy');
-    });
+    Route::controller('UserController')
+        ->prefix('clientes/')
+        ->group(function () {
+            Route::get('lista', 'index')->name('user.index');
+            Route::post('registro', 'store')->name('user.store');
+            Route::put('modificar/{id}', 'update')->name('user.update');
+            Route::delete('eliminar/{id}', 'destroy')->name('user.destroy');
+        });
 
     Route::controller('UserController')->prefix('clientes/')->group(function () {
         Route::get('lista',  'index')->name('user.index');
@@ -55,4 +62,5 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::put('modificar/{id}', 'update')->name('record.update');
         Route::delete('eliminar/{id}', 'destroy')->name('record.destroy');
     });
+    Route::resource('Membership-type', MembershipTypeController::class);
 });
