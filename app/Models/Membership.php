4128436e4dd78acd68a8f0bc6fbb2ea41320b673
Pedro Adrián Sánchez Cárdenas <pedrosanchezcardenas@gmail.com>
2022-10-06 10:43:55 -0500

    Update Relationship

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -14,11 +14,15 @@ class Membership extends Model
      *
      * @var array<int, string>
      */
-     protected $fillable = [
-        'users_id',
-        'membership_types_id',
-        'init_date',
-        'expiration_date',
-        'asigned_by'
-    ];
+    protected $fillable = ['users_id', 'membership_types_id', 'init_date', 'expiration_date', 'asigned_by'];
+
+    /**
+     * Get the MembershipType associated with the Membership
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function MembershipType(): HasMany
+    {
+        return $this->hasMany(MembershipType::class, 'id', 'membership_types_id');
+    }
 }
--- app/Models/MembershipType.php
+++ app/Models/MembershipType.php
@@ -15,4 +15,14 @@ class MembershipType extends Model
      * @var array<int, string>
      */
     protected $fillable = ['name', 'price'];
+
+    /**
+     * Get the Membership that owns the MembershipType
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Membership(): BelongsTo
+    {
+        return $this->belongsTo(Membership::class, 'membership_types_id', 'id');
+    }
 }
