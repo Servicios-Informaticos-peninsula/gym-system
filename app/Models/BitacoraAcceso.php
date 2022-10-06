25a09003d259ea028bc761f7df49073a1e3bfe80
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2022-10-06 14:12:31 -0500

    s

--- app/Models/BitacoraAcceso.php
+++ app/Models/BitacoraAcceso.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 //hoola
+//hpojasd
 class BitacoraAcceso extends Model
 {
     use HasFactory;
