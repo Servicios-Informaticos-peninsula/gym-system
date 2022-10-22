ef8aa394202cb7841ea4e49af848d5ca8774d9e5
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2022-10-22 18:34:55 -0500

    rutas fixed

--- routes/ajax/rutas.php
+++ routes/ajax/rutas.php
@@ -7,17 +7,8 @@ Route::controller('RecordController')->prefix('usuarios/')->group(function () {
     Route::post('/datos/list', 'getData')->name('data.get');
     Route::post('/expediente/list', 'getRecord')->name('record.get');
 
-<<<<<<< HEAD
-});
-Route::get('pdf/expediente/{id}', 'RecordController@show')->name('expediente.pdf');
-Route::get('fotos/pdf/expediente/{id}', 'RecordController@showImg')->name('expediente.fotos');
-Route::post('products/sale', 'SalesController@search')->name('search.products');
-Route::post('products/cashPayment', 'SalesController@cashPayment')->name('cash.payment');
-Route::get('sales/tickets/{id}', 'SalesController@show');
-=======
     });
     Route::get('pdf/expediente/{id}','RecordController@show')->name('expediente.pdf');
     Route::get('fotos/pdf/expediente/{id}','RecordController@showImg')->name('expediente.fotos');
     Route::post('products/sale', 'SalesController@search')->name('search.products');
     Route::post('products/cashPayment', 'SalesController@cashPayment')->name('cash.payment');
->>>>>>> origin/cosme_cambios
