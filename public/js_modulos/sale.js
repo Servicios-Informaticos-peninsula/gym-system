9a52dff54f40aab2cca8e39669a6f55f2303516f
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2022-10-22 20:43:07 -0500

    fixed

--- public/js_modulos/sale.js
+++ public/js_modulos/sale.js
@@ -436,7 +436,7 @@ function calcularCambio() {
 
             $("#cantidad_pagada").val("");
             $("#cambio").val("");
-
+//
         } else {
             $("#cambio").val(Number(cambio).toFixed(2));
         }
